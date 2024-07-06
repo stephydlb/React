cbba1715dcd4f89574700a9f1f7dc0d77885fc02
stephydlb <stephkalubiaka@gmail.com>
2024-07-06 19:08:55 +0100

    member2-member

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
-import Member from './components/Member';
+//import Member from './components/Member';
+
+const famille = {
+  member1:{
+    name: 'stephane',
+    age: 23
+  },
+  member2:{
+    name: 'kalubiaka',
+    age: 24
+  
+  } 
+}
 
 class App extends Component {
 
+  state = {
+    famille
+  }
+
   handleClick = () => {
-    console.log('clicked');
+    const famille = { ...this.state.famille}
+    famille.member1.age += 2
+    famille.member2.age += 2
+    this.setState({famille})
   }
 
   render() {
+    const {famille} = this.state
     return (
       <div className="App">
-        <h1>React application</h1>
-        <Member name="stephane" age={23}/>
-        <Member name="kalubiaka" age={24}/>
-        <Member name="mulumba" age={23}/>
+        <h2>Les Membres de la famille</h2>
+
+        <member>
+          age: {famille.member1.age}
+          name: {famille.member1.name}
+        </member>
+          age: {famille.member2.age}
+          name: {famille.member2.name}<member/>
+        
         <button onClick={this.handleClick}>Click-ici</button>
       </div>
     );
