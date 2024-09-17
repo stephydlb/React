13a21b4991041210ff71fa28c26c0a349e83a039
Stephydlb <stephkalubiaka@gmail.com>
2024-09-18 00:03:37 +0100

    test2

--- chat_box_app/src/App.js
+++ chat_box_app/src/App.js
@@ -1,19 +1,8 @@
-/**
- * The main React component for the application.
- * 
- * This component renders the main UI for the application, including a message display and a form for adding new messages.
- * 
- * The `messages` state holds an object of message data, where the keys are timestamps and the values are the message objects.
- * 
- * The `addMessage` function is used to add a new message to the `messages` state.
- */
 import React, { Component } from 'react'
 import './App.css' 
 
 import Formulaire from './Components/Formulaire'
 import Message from './Components/Message'
-
-
 
 export default class App extends Component {
   state = {
