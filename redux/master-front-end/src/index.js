9cc11e4763ad85cf7ddf5818563d7cc96b70cf6a
Stephydlb <stephkalubiaka@gmail.com>
2024-09-20 12:17:28 +0100

    configurations reducers

--- redux/master-front-end/src/index.js
+++ redux/master-front-end/src/index.js
@@ -3,4 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// REDUX
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers";
+
+const store = configureStore({
+  reducer: rootReducer,
+  devtools: true,
+});
+
+// REDUX STORE
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  document.getElementById("root")
+);
+
